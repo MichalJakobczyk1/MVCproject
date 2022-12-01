4c1c911efe3a56bfe2f2acc733455e6d38ce3316
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2022-12-01 08:51:27 +0100

    Worker model,view and controller todo: fix add button

---
+++ Controllers/WorkerController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCproject.Models;
+
+namespace MVCproject.Controllers
+{
+    public class WorkerController : Controller
+    {
+        public List<Worker> workers = new List<Worker>() 
+        { 
+            new Worker() { id = 1, hiredOn = new DateTime(2020,01,13),name = "Michał",surname="Nowak",contactNumber="512456890",email="nowak@gmail.com"}
+        };
+        [HttpPost]
+        public IActionResult Add([FromForm] Worker worker)
+        {
+            if (ModelState.IsValid)
+            {
+                int counter = workers.Count();
+                worker.id = counter++;
+                workers.Add(worker);
+                return View("Index", workers);
+            }
+
+            return View();
+        }
+        public IActionResult Edit([FromRoute] int id)
+        {
+            var worker = workers.FirstOrDefault(w => w.id == id);
+            return View(worker);
+        }
+        [HttpPost]
+        public IActionResult Edit([FromForm] Worker worker, [FromRoute] int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var index = workers.FindIndex(w => w.id == id);
+                workers[index] = worker;
+                return View("Index", workers);
+            }
+            return View();
+        }
+
+        public IActionResult Delete([FromRoute] int id)
+        {
+            Worker w = workers.FirstOrDefault(w => w.id == id);
+            if (w != null)
+            {
+                workers.Remove(w);
+            }
+            return View("Index", workers);
+        }
+        public IActionResult Index()
+        {
+            return View(workers);
+        }
+    }
+}
---
+++ Models/WorkerModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCproject.Models
+{
+    public class Worker
+    {
+        [HiddenInput]
+        public int id { get; set; }
+        [Required(ErrorMessage = "Date of hire is required")]
+        public DateTime hiredOn { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string name { get; set; }
+        [Required(ErrorMessage = "Surname is required")]
+        public string surname { get; set; }
+        [Required(ErrorMessage = "Age is required")]
+        public int age { get; set; }
+        [RegularExpression("^[0-9]*$")]
+        [Required(ErrorMessage = "Contact number is required")]
+        public string contactNumber { get; set; }
+        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
+        public string email { get; set; }
+    }
+}
