64a87c4da0d1cec8bd08d3df62e5ba49b0c1994e
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2023-01-18 14:11:12 +0100

    employee create view&controller

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,13 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCproject.Interfaces;
 using MVCproject.Models;
+using MVCproject.ViewModels;
+using System.Net;
 
 namespace MVCproject.Controllers
 {
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
+        private readonly IEmployeeRepository _employeeRepository;
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Employee> employees = await _employeeRepository.GetAll();
+            return View(employees);
+        }
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(employee);
+            }
+            _employeeRepository.Add(employee);
+            return RedirectToAction("Index");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using MVCproject.Data;
+using MVCproject.Interfaces;
+using MVCproject.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
