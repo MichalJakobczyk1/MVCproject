541aaa60d90e62009b21e43ec4b9059699e5cc77
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2023-01-18 13:43:56 +0100

    club repo&interface

---
+++ Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using MVCproject.Models;
+
+namespace MVCproject.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAll();
+        Task<Employee> GetByIdAsync(int id);
+        Task<Employee> GetByIdAsyncNoTracking(int id);
+        bool Add(Employee employee);
+        bool Update(Employee employee);
+        bool Delete(Employee employee);
+        bool Save();
+    }
+}
---
+++ Repositories/EmployeeRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCproject.Data;
+using MVCproject.Interfaces;
+using MVCproject.Models;
+
+namespace MVCproject.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Employee employee)
+        {
+            _context.Add(employee);
+            return Save();
+        }
+
+        public bool Delete(Employee employee)
+        {
+            _context.Remove(employee);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Employee>> GetAll()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        public async Task<Employee> GetByIdAsync(int id)
+        {
+            return await _context.Employees.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task<Employee> GetByIdAsyncNoTracking(int id)
+        {
+            return await _context.Employees.AsNoTracking().FirstOrDefaultAsync(i => i.Id == id);
+        }
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(Employee employee)
+        {
+            _context.Update(employee);
+            return Save();
+        }
+    }
+}
